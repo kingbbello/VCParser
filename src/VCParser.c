f2e446c0eca089e6f4e7352841efa086dc83100e
Mustapha Bello <mbello@fred.socs.uoguelph.ca>
2020-09-25 14:23:41 -0400

    finished working on card members(fn and optionalProperties). Problem with initializing optionallist

--- src/VCParser.c
+++ src/VCParser.c
@@ -1,8 +1,9 @@
+#define _GNU_SOURCE
 #include <stdbool.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "VCParser.h"
+#include "../include/VCParser.h"
 
 char *readFile(char *fileName)
 {
@@ -110,6 +111,14 @@ void processGroup(Property *theProp, char *subString)
 
 void processValue(Property *theProp, char *subString)
 {
+    // char *token = strtok(subString, "=");
+
+    //     while (token != NULL)
+    //     {
+
+    // printf("%s \n", token);
+    //         token = strtok(NULL, " ");
+    //     }
 }
 
 void processParam(Property *theProp, char *subString)
@@ -118,8 +127,6 @@ void processParam(Property *theProp, char *subString)
 //Takes a line from VCF and puts puts the appropriate date-time or Property into the Card
 void processProp(Card *thecard, char *contentLine)
 {
-    Property *prop = malloc(sizeof(Property));
-    prop->parameters = NULL;
     char *part;
     char *part2;
     char *part3;
@@ -182,20 +189,97 @@ void processProp(Card *thecard, char *contentLine)
         strcpy(part, "");
     }
 
-    printf("1 -> %s \n", part);
-    printf("2 -> %s \n", part2);
-    printf("3 -> %s \n", part3);
-    printf("4 -> %s \n\n", part4);
+    // printf("1 -> %s \n", part);
+    // printf("2 -> %s \n", part2);
+    // printf("3 -> %s \n", part3);
+    // printf("4 -> %s \n\n", part4);
 
-    // thecard->anniversary = malloc(sizeof(DateTime));
-    // thecard->birthday = malloc(sizeof(DateTime));
-    // thecard->fn = malloc(sizeof(Property));
-    // thecard->optionalProperties = initializeList(&propertyToString, &deleteProperty, &compareProperties);
-    free(part);
-    free(part2);
-    free(part3);
-    free(part4);
-    free(prop);
+    //Wokring on Properties
+    if (strcmp(part2, "anniversary") != 0 && strcmp(part2, "birthday") != 0)
+    {
+        Property *prop = malloc(sizeof(Property));
+        prop->group = part;
+        prop->name = part2;
+        prop->values = initializeList(&valueToString, &deleteValue, &compareValues);
+        prop->parameters = initializeList(&parameterToString, &deleteParameter, &compareParameters);
+
+        //Get the parameters for all contentline
+        char *end_str;
+        char *token = strtok_r(part3, ";", &end_str);
+        while (token != NULL)
+        {
+            Parameter *param = malloc(sizeof(Parameter));
+            char *end_token;
+            char *token2 = strtok_r(token, "=", &end_token);
+            while (token2 != NULL)
+            {
+                param->name = malloc(strlen(token2) + 1);
+                strcpy(param->name, token2);
+                // printf("name = %s\n", param->name);
+
+                token2 = strtok_r(NULL, "=", &end_token);
+                param->value = malloc(strlen(token2) + 1);
+                strcpy(param->value, token2);
+                // printf("value = %s\n", param->value);
+                insertBack(prop->parameters, param);
+                break;
+            }
+
+            token = strtok_r(NULL, ";", &end_str);
+        }
+
+        //Getting all values from contentline
+
+        int start = 0;
+
+        for (int i = 0; i < strlen(part4); i++)
+        {
+            if (part4[i] == ';' || part4[i + 1] == '\0')
+            {
+                char *value = substring(part4, start, i);
+
+                if (value[0] == ';')
+                {
+                    char *string = malloc(2);
+                    strcpy(string, "");
+                    insertBack(prop->values, string);
+                    printf("empty value \n");
+                }
+                else
+                {
+                    if (value[strlen(value) - 1] == ';')
+                    {
+                        value[strlen(value) - 1] = '\0';
+                    }
+                    char *string = malloc(strlen(value) + 1);
+                    strcpy(string, value);
+                    insertBack(prop->values, string);
+                    printf("%s \n", value);
+                }
+
+                start = i + 1;
+                free(value);
+            }
+        }
+        printf("\n");
+        free(part4);
+        free(part3);
+
+        if(strcmp(prop->name, "FN") == 0){
+            thecard->fn = prop;
+        }else{
+            insertBack(thecard->optionalProperties, prop);
+        }
+        deleteProperty(prop);
+    }
+    else
+    {
+    }
+
+    // free(part);
+    // free(part2);
+    // free(part3);
+    // free(part4);
 }
 
 VCardErrorCode createCard(char *fileName, Card **newCardObject)
@@ -221,6 +305,7 @@ VCardErrorCode createCard(char *fileName, Card **newCardObject)
             if (seen == 1)
             {
                 theCard = malloc(sizeof(Card));
+                theCard->optionalProperties = initializeList(&propertyToString, &deleteProperty, &compareProperties);
             }
             else if (seen == 2)
             {
@@ -238,6 +323,7 @@ VCardErrorCode createCard(char *fileName, Card **newCardObject)
 
     // deleteCard(theCard);
     free(theCard);
+    // freeList(theCard->optionalProperties);
     free(newCardObject);
     free(text);
     return OK;
